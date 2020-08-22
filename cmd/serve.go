431fdcdb84bd49fb198eca25e4dc1bdb0e9ffea9
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-08-22 16:19:32 -0400

    setup a serve command

---
+++ cmd/serve.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start server",
+	Long:  `Serves your files and folders`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	serveCmd.Flags().StringP("path", "p", ".", "Specifies which path to serve files from")
+}
